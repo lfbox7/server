54570da663fec37feeaad3f7b1ff24b7abe4902d
Leonard Box <lfbox7@yahoo.com>
2022-04-03 10:54:28 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+require('dotenv').config();
+
+// routers
+const routes = require('./routes/routes');
+
+// middleware applications
+const app = express();
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/', routes);
+
+app.listen(process.env.PORT, () => {
+    console.log('Express server is running!');
+});
