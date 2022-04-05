fb2ba562f3d710ad4706502b2baf32786927a8af
Leonard Box <leonard.box@tlgcohort.com>
2022-04-05 20:42:39 +0000

    mixed lists

---
+++ day2warmup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''
+Alta3 Research | LFBox
+Day 2 Warmup
+'''
+
+def main():
+    pi = 3.14
+    c = 'circumference'
+    r = 'radius'
+
+    print(f'To calculate the {c} of a circle, the formula is 2 x {pi} x {r}')
+    # OUTPUT SHOULD READ AS:
+    # To calculate the circumference of a circle, the formula is 2 x 3..14 x radius
+
+# calling main()
+if __name__ == '__main__':
+    main()
